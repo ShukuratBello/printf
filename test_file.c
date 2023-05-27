c895114c99283a62281647d0f471e8828e7339b1
ImoleFavour <imoleokediji@gmail.com>
2023-05-27 10:58:22 -0700

    test_file.c

---
+++ test_file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Description: Int, char variables
+ * Retrn: Always 0
+ */
+void int_main(char)
+{
+	char = "This is a test file";
+	
+	return = 0;
+}			
