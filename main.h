42579791ad7a34008be463f3c5c860ee5ee0ae84
ImoleFavour <imoleokediji@gmail.com>
2023-05-29 05:41:23 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,22 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct type - structure for printing various format types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct type
+{
+	char *t;
+	int (*f)(va_list);
+} 
+type_t
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int type_c(va_list c);
+int type_s(va_list s);
+int type_p(va_list p);
 #endif
